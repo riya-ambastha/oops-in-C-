abcd22840eaa89ac2b5c1da06ad44fc3428451ba
Riya Ambastha <ambasthariya58@gmail.com>
2024-07-07 09:07:44 +0530

    Add files via upload

---
+++ deepCopy.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class student{
+public:
+    string name;
+    double* cgpaPtr;
+
+    student(string name, double cgpa) {
+        this->name = name;
+        cgpaPtr = new double;
+        *cgpaPtr = cgpa;
+    }
+
+    student(student &obj) {
+        this->name = obj.name;
+        cgpaPtr = new double;
+        *cgpaPtr = *obj.cgpaPtr;
+    }
+
+    void getinfo() {
+        cout<<"name:"<<name<<endl;
+         cout<<"cgpa:"<<*cgpaPtr<<endl;
+    }
+};
+
+int main() {
+    student s1("rahul" , 8.9);
+    student s2(s1);
+
+     s1.getinfo();
+     *(s2.cgpaPtr)= 9.2;
+     s2.name="riya";
+     s1.getinfo();
+     s2.getinfo();
+    return 0;
+}
